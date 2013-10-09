ee1dc7b50fc11852388747993de63a850d7e9960
Oliver Godby <oliver_godby@me.com>
2013-10-09 15:54:31 +0100

    Validation methods complete
    Individual validation methods completed.
    
    Signed-off-by: Oliver Godby <oliver_godby@me.com>

--- app/js/Validator.js
+++ app/js/Validator.js
@@ -1,5 +1,39 @@
-function Validator() {}
+function Validator() {
+    this.allowedsymbols = {first : ["£"], last : ["p"]};
+}
 
-Validator.prototype.validate = function(input) {
-  x = 1;  
+Validator.prototype.validate_numeric = function(input) {
+  is_numeric = /^-?\d+\.?\d*$/;
+  if (is_numeric.test(input)) {
+      return true;
+  } else {
+      return false;
+  }
 };
+
+Validator.prototype.validate_allowedsymbols = function(input, allowedsymbols) {
+    for (var i = 0; i < this.allowedsymbols.first.length; i++) {
+        var reg = new RegExp('(^' + this.allowedsymbols.first[i] + ').*');
+        if (reg.test(input)) {
+            input = input.substr(1,input.length);
+        }
+    }
+    for (var j = 0; j < this.allowedsymbols.last.length; j++) {
+        var reg = new RegExp('.*(' + this.allowedsymbols.last[j] + '$)');
+        if (reg.test(input)) {
+            input = input.slice(0,-1);
+        }
+    }
+    return input;
+}
+
+Validator.prototype.validate_convertfloat = function(input) {
+    String(input);
+    reg = /^[\d]*\.$/;
+    if ((input % 1 === 0) && (reg.test(input) === false)) {
+        output = parseInt(input);
+    } else {
+        output = parseFloat(input).toFixed(2) * 100;
+    }
+    return Math.round(output);
+}
--- app/test/spec/ValidatorSpec.js
+++ app/test/spec/ValidatorSpec.js
@@ -5,8 +5,43 @@ describe("Validator", function() {
     validator = new Validator();
   });
   
-  it("should have a method to validate input", function() {
-     expect(validator.validate).toBeDefined(); 
+  it("should have a list of allowed symbols by position", function() {
+     expect(validator.allowedsymbols).toEqual({first : ["£"], last : ["p"]}); 
+  });
+  
+  it("should have a method to validate input as numeric", function() {
+     expect(validator.validate_numeric).toBeDefined();
+  });
+  
+  it("should validate input as whole numbers and floating point numbers", function() {
+     expect(validator.validate_numeric("21")).toBeTruthy;
+     expect(validator.validate_numeric("0.21")).toBeTruthy;
+     expect(validator.validate_numeric("0.2199825783496534278")).toBeTruthy;
+     expect(validator.validate_numeric("2x1")).not.toBeTruthy;
+     expect(validator.validate_numeric("2x1.54787")).not.toBeTruthy;
+     expect(validator.validate_numeric("21.")).toBeTruthy;
+     expect(validator.validate_numeric(" ")).not.toBeTruthy;
+     expect(validator.validate_numeric("£p")).not.toBeTruthy;
+  });
+  
+  it("should have a method to detect and strip allowed symbols", function() {
+     expect(validator.validate_allowedsymbols).toBeDefined();
+     expect(validator.validate_allowedsymbols("£1.24p", validator.allowedsymbols)).toEqual("1.24");
+     expect(validator.validate_allowedsymbols("£1@.24p", validator.allowedsymbols)).toEqual("1@.24");
+     expect(validator.validate_allowedsymbols("£1.24px", validator.allowedsymbols)).toEqual("1.24px");
+     expect(validator.validate_allowedsymbols("£1.24", validator.allowedsymbols)).toEqual("1.24");
+     expect(validator.validate_allowedsymbols("1.24p", validator.allowedsymbols)).toEqual("1.24");
+     expect(validator.validate_allowedsymbols("£p", validator.allowedsymbols)).toEqual("");
+  });
+  
+  it("should have a method to detect floating point numbers and convert to pence", function() {
+     expect(validator.validate_convertfloat).toBeDefined();
+     expect(validator.validate_convertfloat("12")).toEqual(12);
+     expect(validator.validate_convertfloat("1.2")).toEqual(120);
+     expect(validator.validate_convertfloat("1.28454382568947534285702345")).toEqual(128);
+     expect(validator.validate_convertfloat("1.28654382568947534285702345")).toEqual(129);
+     expect(validator.validate_convertfloat("1.")).toEqual(100);
+     expect(validator.validate_convertfloat("1.1")).toEqual(110); 
   });
   
 });
