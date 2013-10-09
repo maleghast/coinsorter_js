aeb32e223b6a60aef96e6f0b9c6f9b5997759849
Oliver Godby <oliver_godby@me.com>
2013-10-09 10:41:39 +0100

    Initial Function Created
    Created function to manage division, in order to facilitate the construction of a greedy algorithm to solve the problem.
    
    Signed-off-by: Oliver Godby <oliver_godby@me.com>

--- app/js/CoinSorter.js
+++ app/js/CoinSorter.js
@@ -11,3 +11,15 @@ function CoinSorter() {
     };
     this.coinlist = [200,100,50,20,10,5,2,1];
 }
+
+//Create a method / function to test clean division and return relevant data to allow
+//for creation of greedy algorithm solution based on functional composition.
+
+CoinSorter.prototype.dividescleanby = function(amount, divisor) {
+    remainder = amount % divisor;
+    if (remainder === 0) {
+        return {"dividesclean":true,"coin":divisor,"quotient":(amount/divisor),"remainder":remainder};
+    } else {
+        return {"dividesclean":false,"coin":divisor,"quotient":Math.floor(amount/divisor),"remainder":remainder};
+    }
+};
--- app/test/spec/CoinSorterSpec.js
+++ app/test/spec/CoinSorterSpec.js
@@ -6,10 +6,16 @@ describe("CoinSorter", function() {
   });
   
   it("should have a map of coin values to labels and a list of coins as properties", function() {
-     expect(coinsorter.coinmap).toBeDefined();
-     expect(coinsorter.coinlist).toBeDefined();
-     expect(coinsorter.coinmap).toEqual({200:"£2",100:"£1",50:"50p",20:"20p",10:"10p",5:"5p",2:"2p",1:"1p"}); 
-     expect(coinsorter.coinlist).toEqual([200,100,50,20,10,5,2,1]); 
+    expect(coinsorter.coinmap).toBeDefined();
+    expect(coinsorter.coinlist).toBeDefined();
+    expect(coinsorter.coinmap).toEqual({200:"£2",100:"£1",50:"50p",20:"20p",10:"10p",5:"5p",2:"2p",1:"1p"}); 
+    expect(coinsorter.coinlist).toEqual([200,100,50,20,10,5,2,1]); 
+  });
+  
+  it("should have a function for testing clean division", function() {
+    expect(coinsorter.dividescleanby).toBeDefined();
+    expect(coinsorter.dividescleanby(200,100)).toEqual({"dividesclean":true,"coin":100,"quotient":2,"remainder":0});
+    expect(coinsorter.dividescleanby(212,100)).toEqual({"dividesclean":false,"coin":100,"quotient":2,"remainder":12})
   });
   
 });
